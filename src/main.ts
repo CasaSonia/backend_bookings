96926b0d510a12dda48c05052d08358138466232
FacundoBraida98 <facundobraida98@gmail.com>
2024-04-16 13:38:31 -0300

    hostname added

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { hostname } from 'os';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT, '0.0.0.0');
 }
 bootstrap();
